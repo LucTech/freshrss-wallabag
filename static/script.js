50abf41836a773eeda7601ca488c4ff5ae67a5f4
Christian Putzke <christian@graviox.com>
2020-11-13 22:48:56 +0100

    Added simple hardcoded keyboard shortcut

--- static/script.js
+++ static/script.js
@@ -1,40 +1,61 @@
 function save_wallabag(active) {
     if (active.length === 0) {
-	return false;
+        return false;
     }
 
     var wallabagBtn = active.find("a.wallabag");
     var url = wallabagBtn.attr("href");
     if (url === undefined) {
-	return false;
+        return false;
     }
     wallabagBtn.text('.');
 
     if (pending_entries[active.attr('id')]) {
-	return false;
+        return false;
     }
     pending_entries[active.attr('id')] = true;
 
     $.ajax({
-	type: 'POST',
-	url: url,
-	data : {
-	    ajax: true,
-	    _csrf: context.csrf,
-	},
+        type: 'POST',
+        url: url,
+        data: {
+            ajax: true,
+            _csrf: context.csrf,
+        },
     }).done(function (data) {
-	delete pending_entries[active.attr('id')];
+        delete pending_entries[active.attr('id')];
         wallabagBtn.text('✓');
     }).fail(function (data) {
-	openNotification(i18n.notif_request_failed, 'bad');
-	delete pending_entries[active.attr('id')];
+        openNotification(i18n.notif_request_failed, 'bad');
+        delete pending_entries[active.attr('id')];
     });
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('#stream .flux a.wallabag').on('click', function () {
         var active = $(this).parents(".flux");
         save_wallabag(active);
         return false;
+    });
+});
+
+$(document).ready(function () {
+    $('#stream .flux a.wallabag').on('click', function () {
+        var active = $(this).parents(".flux");
+        save_wallabag(active);
+        return false;
+    });
+
+    $(this).keydown(function (e) {
+        if (e.target.closest('input, textarea') || e.ctrlKey || e.metaKey || e.altKey || e.shiftKey) {
+            return true;
+        }
+
+        var active = $('#stream .flux.active');
+        var shortcut = 'i';
+
+        if (e.key == shortcut) {
+            save_wallabag(active);
+        }
     });
 });
